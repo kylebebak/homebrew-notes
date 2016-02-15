09bd2b495e1755496bc7e8048d1c1f4623c9ec6c
Kyle Bebak <kylebebak@gmail.com>
2016-02-15 09:48:57 -0600

    updated sha for new package

--- Formula/notes.rb
+++ Formula/notes.rb
@@ -1,11 +1,14 @@
 class Notes < Formula
-  desc "notes is a command line program that provides quick access, editing capability, and search functionality to all notes under a directory of your choosing. Under your notes directory, you can organize your notes into any folder structure you like."
+  desc "CLI tool for quick access, editing capability, and search functionality to all notes under a directory of your choosing. Under your notes directory, organize your notes into any folder structure you like."
   homepage "https://github.com/kylebebak/notes"
   url "https://github.com/kylebebak/notes/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "7169616e338bd04679cc83176f15a7d0976f0fa1e53ebdccf32fcbb6fcc4d9fd"
+  sha256 "cea485e5dd8d5d027f0781f0d13cffa783d8012ba41eedaec9874fb6ccf8cb6a"
 
   def install
-    bin.install "notes"
+    bin.install "bin/notes"
+    prefix.install Dir["_completions"]
+    prefix.install Dir["_helpers"]
+    prefix.install Dir["_config"]
   end
 end
