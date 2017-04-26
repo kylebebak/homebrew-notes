a517df71bf1201bbc243ded94ea773cc9083dd1d
Kyle Bebak <kylebebak@gmail.com>
2017-04-26 00:27:50 -0500

    Updated formula to point to new version

--- Formula/notes.rb
+++ Formula/notes.rb
@@ -1,9 +1,9 @@
 class Notes < Formula
   desc "CLI tool for quick access, editing capability, and search functionality to all notes under a directory of your choosing. Under your notes directory, organize your notes into any folder structure you like."
   homepage "https://github.com/kylebebak/notes"
-  url "https://github.com/kylebebak/notes/archive/1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "e17405adc655824dec3ca6e2a9a4b199a715743ed5f0948df58f6bb369267aa3"
+  url "https://github.com/kylebebak/notes/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "a06a2e6bd8a7d5d476644a9343f0dc4686b58151007c80c34f733024c16eca67"
 
   def install
     bin.install "bin/notes"
